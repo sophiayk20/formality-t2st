9045d744baecc7801e6d6477b1a502ada371f42b
syk2021 <sophia.kang@yale.edu>
2025-04-07 14:12:22 -0400

    Add model testing code.

---
+++ src/testing/model_testing.py
@@ -0,0 +1,24 @@
+"""
+    Code for checking if TTS model produces valid speech.
+    `pip install -q datasets`
+"""
+
+from transformers import SpeechT5ForTextToSpeech, SpeechT5Processor
+from datasets import load_dataset
+from transformers import SpeechT5HifiGan
+from IPython.display import Audio
+import torch
+
+processor=SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+model = SpeechT5ForTextToSpeech.from_pretrained(
+    "sophiayk20/speecht5_tts_formal_es"
+)
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split='validation')
+speaker_embeddings = torch.tensor(embeddings_dataset[2010]["xvector"]).unsqueeze(0)
+text="Hola me llamo Sabrina y esto es una frase en espanol."
+inputs = processor(text=text, return_tensors="pt")
+
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+
+Audio(speech.numpy(), rate=16000)
